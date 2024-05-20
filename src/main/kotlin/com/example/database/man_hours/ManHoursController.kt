c9d96257f8336d0646a654591270cf03cbc2b393
kurcevroman11 <kurcevroman11@gmail.com>
2024-05-20 15:49:21 +0300

    Добавлена сумма трудозатрат

--- src/main/kotlin/com/example/database/man_hours/ManHoursController.kt
+++ src/main/kotlin/com/example/database/man_hours/ManHoursController.kt
@@ -16,6 +16,8 @@ import org.apache.poi.ss.usermodel.HorizontalAlignment
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import java.io.File
 import java.io.FileOutputStream
+import java.time.Duration
+import java.time.OffsetDateTime
 
 class ManHoursController(val call: ApplicationCall) {
     suspend fun insertManHours(){
@@ -75,10 +77,44 @@ class ManHoursController(val call: ApplicationCall) {
         val manHoursId = call.parameters["projId"]?.toInt()
 
         if(manHoursId != null){
-            call.respond(fetchByProjectId(manHoursId))
+            val manHour = fetchByProjectId(manHoursId)
+            val aggregatedHours = aggregateHoursSpent(manHour)
+            call.respond(aggregatedHours)
         } else {
             call.respond(HttpStatusCode.BadRequest, "Invalid ID")
         }
+    }
+
+    fun parseDuration(time: String?): Duration {
+        if (time == null || time.isEmpty()) return Duration.ZERO
+        val parts = time.split(":")
+        val hours = parts[0].toLong()
+        val minutes = parts[1].toLong()
+        return Duration.ofHours(hours).plusMinutes(minutes)
+    }
+
+    fun formatDuration(duration: Duration): String {
+        val hours = duration.toHours()
+        val minutes = duration.minusHours(hours).toMinutes()
+        return "%02d:%02d".format(hours, minutes)
+    }
+
+    fun aggregateHoursSpent(manHours: List<ManHoursReportDTO>): List<ManHoursReportDTO> {
+        return manHours
+            .filter { it.createdAt != null && it.hoursSpent != null }
+            .groupBy { OffsetDateTime.parse(it.createdAt).toLocalDate().toString() }
+            .map { (date, entries) ->
+                val totalDuration = entries
+                    .map { parseDuration(it.hoursSpent) }
+                    .reduce { acc, duration -> acc.plus(duration) }
+                ManHoursReportDTO(
+                    id = entries.first().id, // или другой логики для id
+                    createdAt = date,
+                    hoursSpent = formatDuration(totalDuration),
+                    taskId = entries.first().taskId,
+                    taskName = entries.first().taskName
+                )
+            }
     }
 
     // Функция для создания Excel файла
